f8dbe52c3fc0feb1f337d5132a963670d36c3d51
Арсеньев Константин <kostik.arsenev.95@mail.ru>
2024-10-05 07:24:50 +0300

    -added year leap-checker

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿##
+
+var currentYear := ReadInteger('Введите текущий год: ');
+if (currentYear mod 4 = 0) and not((currentYear mod 100 = 0) and (currentYear mod 400 <> 0)) then
+  println('Текущий год високосный.')
+else
+  println('Текущий год не високосный');
