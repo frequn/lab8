cc81a27ea1ebc8ad2f5a747421d67a844b1980cb
Арсеньев Константин <kostik.arsenev.95@mail.ru>
2024-10-05 07:31:00 +0300

    time.pas -added days/year counter

--- time.pas
+++ time.pas
@@ -1,7 +1,11 @@
 ﻿##
 
 var currentYear := ReadInteger('Введите текущий год: ');
-if (currentYear mod 4 = 0) and not((currentYear mod 100 = 0) and (currentYear mod 400 <> 0)) then
+if (currentYear mod 4 = 0) and not((currentYear mod 100 = 0) and (currentYear mod 400 <> 0)) then begin
   println('Текущий год високосный.')
-else
+  println('Количество дней в году: 366.');
+end
+else begin
   println('Текущий год не високосный');
+  println('Количество дней в году: 365.');
+end;
---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿##
+
+println('Введите количество часов, минут и секунд: ');
+var (hours, minutes, seconds) := (ReadInteger(), ReadInteger(), ReadInteger());
+
+println($'Суммарное количество секунд: {seconds + minutes*60 + hours*3600}.');
